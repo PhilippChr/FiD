b04297eebf67af0b198a8ab9c57703e2565cd908
Philipp Christmann <pchristm@mpi-inf.mpg.de>
2022-05-20 08:40:58 +0200

    Enable running on CPU

--- test_reader.py
+++ test_reader.py
@@ -1,6 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
-# 
+#
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
@@ -19,6 +19,7 @@ import src.data
 import src.evaluation
 import src.model
 
+
 def evaluate(model, dataset, dataloader, tokenizer, opt):
     loss, curr_loss = 0.0, 0.0
     model.eval()
@@ -26,12 +27,12 @@ def evaluate(model, dataset, dataloader, tokenizer, opt):
         model = model.module
     if opt.write_crossattention_scores:
         model.overwrite_forward_crossattention()
-        model.reset_score_storage() 
+        model.reset_score_storage()
     total = 0
     exactmatch = []
     if opt.write_results:
-        write_path = Path(opt.checkpoint_dir) / opt.name / 'test_results'
-        fw = open(write_path / ('%d.txt'%opt.global_rank), 'a')
+        write_path = Path(opt.checkpoint_dir) / opt.name / "test_results"
+        fw = open(write_path / ("%d.txt" % opt.global_rank), "a")
     with torch.no_grad():
         for i, batch in enumerate(dataloader):
             (idx, _, _, context_ids, context_mask) = batch
@@ -39,42 +40,54 @@ def evaluate(model, dataset, dataloader, tokenizer, opt):
             if opt.write_crossattention_scores:
                 model.reset_score_storage()
 
-            outputs = model.generate(
-                input_ids=context_ids.cuda(),
-                attention_mask=context_mask.cuda(),
-                max_length=50,
-            )
+            if torch.cuda.is_available():
+                outputs = model.generate(
+                    input_ids=context_ids.cuda(),
+                    attention_mask=context_mask.cuda(),
+                    max_length=50,
+                )
+            else:
+                outputs = model.generate(
+                    input_ids=context_ids,
+                    attention_mask=context_mask,
+                    max_length=50,
+                )
 
             if opt.write_crossattention_scores:
-                crossattention_scores = model.get_crossattention_scores(context_mask.cuda())
+                if torch.cuda.is_available():
+                    crossattention_scores = model.get_crossattention_scores(context_mask.cuda())
+                else:
+                    crossattention_scores = model.get_crossattention_scores(context_mask)
 
             for k, o in enumerate(outputs):
                 ans = tokenizer.decode(o, skip_special_tokens=True)
                 example = dataset.data[idx[k]]
-                if 'answers' in example:
-                    score = src.evaluation.ems(ans, example['answers'])
+                if "answers" in example:
+                    score = src.evaluation.ems(ans, example["answers"])
                     exactmatch.append(score)
 
                 if opt.write_results:
-                    fw.write(str(example['id']) + "\t" + ans + '\n')
+                    fw.write(str(example["id"]) + "\t" + ans + "\n")
                 if opt.write_crossattention_scores:
                     for j in range(context_ids.size(1)):
-                        example['ctxs'][j]['score'] = crossattention_scores[k, j].item()
+                        example["ctxs"][j]["score"] = crossattention_scores[k, j].item()
 
                 total += 1
             if (i + 1) % opt.eval_print_freq == 0:
-                log = f'Process rank:{opt.global_rank}, {i+1} / {len(dataloader)}'
+                log = f"Process rank:{opt.global_rank}, {i+1} / {len(dataloader)}"
                 if len(exactmatch) == 0:
-                    log += '| no answer to compute scores'
+                    log += "| no answer to compute scores"
                 else:
-                    log += f' | average = {np.mean(exactmatch):.3f}'
+                    log += f" | average = {np.mean(exactmatch):.3f}"
                 logger.warning(log)
 
-    logger.warning(f'Process rank:{opt.global_rank}, total {total} | average = {np.mean(exactmatch):.3f}')
+    logger.warning(
+        f"Process rank:{opt.global_rank}, total {total} | average = {np.mean(exactmatch):.3f}"
+    )
     if opt.is_distributed:
         torch.distributed.barrier()
     score, total = src.util.weighted_average(np.mean(exactmatch), total, opt)
-    
+
     return score, total
 
 
@@ -87,40 +100,41 @@ if __name__ == "__main__":
     src.slurm.init_signal_handler()
     opt.train_batch_size = opt.per_gpu_batch_size * max(1, opt.world_size)
 
-    dir_path = Path(opt.checkpoint_dir)/opt.name
+    dir_path = Path(opt.checkpoint_dir) / opt.name
     directory_exists = dir_path.exists()
     if opt.is_distributed:
         torch.distributed.barrier()
     dir_path.mkdir(parents=True, exist_ok=True)
     if opt.write_results:
-        (dir_path / 'test_results').mkdir(parents=True, exist_ok=True)
-    logger = src.util.init_logger(opt.is_main, opt.is_distributed, Path(opt.checkpoint_dir) / opt.name / 'run.log')
+        (dir_path / "test_results").mkdir(parents=True, exist_ok=True)
+    logger = src.util.init_logger(
+        opt.is_main, opt.is_distributed, Path(opt.checkpoint_dir) / opt.name / "run.log"
+    )
     if not directory_exists and opt.is_main:
         options.print_options(opt)
 
+    tokenizer = transformers.T5Tokenizer.from_pretrained("t5-base", return_dict=False)
 
-    tokenizer = transformers.T5Tokenizer.from_pretrained('t5-base', return_dict=False)
-
-    collator_function = src.data.Collator(opt.text_maxlength, tokenizer)
+    collator_function = src.data.Collator(opt.text_maxlength, tokenizer, logger)
     eval_examples = src.data.load_data(
-        opt.eval_data, 
-        global_rank=opt.global_rank, #use the global rank and world size attibutes to split the eval set on multiple gpus
-        world_size=opt.world_size
+        opt.eval_data,
+        global_rank=opt.global_rank,  # use the global rank and world size attibutes to split the eval set on multiple gpus
+        world_size=opt.world_size,
     )
     eval_dataset = src.data.Dataset(
-        eval_examples, 
-        opt.n_context, 
+        eval_examples,
+        opt.n_context,
     )
 
-    eval_sampler = SequentialSampler(eval_dataset) 
+    eval_sampler = SequentialSampler(eval_dataset)
     eval_dataloader = DataLoader(
-        eval_dataset, 
-        sampler=eval_sampler, 
+        eval_dataset,
+        sampler=eval_sampler,
         batch_size=opt.per_gpu_batch_size,
-        num_workers=20, 
-        collate_fn=collator_function
+        num_workers=20,
+        collate_fn=collator_function,
     )
-    
+
     model_class = src.model.FiDT5
     model = model_class.from_pretrained(opt.model_path)
     model = model.to(opt.device)
@@ -128,12 +142,11 @@ if __name__ == "__main__":
     logger.info("Start eval")
     exactmatch, total = evaluate(model, eval_dataset, eval_dataloader, tokenizer, opt)
 
-    logger.info(f'EM {100*exactmatch:.2f}, Total number of example {total}')
+    logger.info(f"EM {100*exactmatch:.2f}, Total number of example {total}")
 
     if opt.write_results and opt.is_main:
-        glob_path = Path(opt.checkpoint_dir) / opt.name / 'test_results'
-        write_path = Path(opt.checkpoint_dir) / opt.name / 'final_output.txt'
-        src.util.write_output(glob_path, write_path) 
+        glob_path = Path(opt.checkpoint_dir) / opt.name / "test_results"
+        write_path = Path(opt.checkpoint_dir) / opt.name / "final_output.txt"
+        src.util.write_output(glob_path, write_path)
     if opt.write_crossattention_scores:
         src.util.save_distributed_dataset(eval_dataset.data, opt)
-
